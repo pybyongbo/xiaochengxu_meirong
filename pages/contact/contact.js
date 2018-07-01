f3f7c82f0f8f0b3fc2ecce1d9a0c7e5007d67f9a
彭永波 <yongbo.peng@molbase.com>
2018-07-01 17:56:15 +0800

    文件新增修改提交

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,109 +1,68 @@
-//add.js
+//contact.js
+
+var bmap = require('../../utils/bmap-wx.min.js');
+var wxMarkerData = [];
+
 Page({
     data: {
-
+        markers: [{
+            longitude: "117.187499",
+            latitude: "39.126306",
+            id: 0,
+            iconPath: '../../images/marker_red.png',
+            iconTapPath: '../../images/marker_red.png'
+        }],
+        latitude: '',
+        longitude: '',
+        rgcData: {}
     },
-
-    onLoad: function(e) {
-
-        var id = e.id;
-        // console.log(id);
-        if (id) {
-            getData(id, this);
-        } else {
-            console.log("test")
-            this.setData({
-                id: Date.now(),
-                txtStyle: "",
-            })
-        }
-
+    makertap: function(e) {
+        var that = this;
+        var id = e.markerId;
+        that.showSearchInfo(wxMarkerData, id);
     },
-    change(e) {
-        var val = e.detail.value;
-        console.log(val);
-        this.setData({
-            content: val
-        })
+    onLoad: function() {
+        var that = this;
+        // 新建百度地图对象 
+        var BMap = new bmap.BMapWX({
+            ak: '4d0be64a393aa984ca02f876f9098e5a'
+        });
+        var fail = function(data) {
+            console.log(data)
+        };
+        var success = function(data) {
+                wxMarkerData = data.wxMarkerData;
+                that.setData({
+                    // markers: wxMarkerData
+                    markers: this.markers[0]
+
+                });
+                that.setData({
+                    latitude: wxMarkerData[0].latitude
+                });
+                that.setData({
+                    longitude: wxMarkerData[0].longitude
+                });
+            }
+            // 发起regeocoding检索请求 
+            // BMap.regeocoding({
+            //     fail: fail,
+            //     success: success,
+            //     iconPath: '../../images/marker_red.png',
+            //     iconTapPath: '../../images/marker_red.png'
+            // });
     },
-    cancel() {
-
-        wx.navigateBack();
-    },
-    sure() {
-        var re = /^\s*$/g;
-        if (!this.data.content || re.test(this.data.content)) {
-            wx.showToast({
-                title: `请填写完整内容在提交`,
-                image: '/images/appointment.png',
-                icon: 'fail'
-
-            })
-            return false
-
-        }
-        this.setData({
-            time: Date.now()
-        })
-
-        setvalue(this);
-        // wx.navigateBack();
-        wx.switchTab({
-            url: '../list/list'
-        })
-    },
-    getUserInfo: function(e) {
-        console.log(e)
-        app.globalData.userInfo = e.detail.userInfo
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
-        })
+    showSearchInfo: function(data, i) {
+        var that = this;
+        console.log(data);
+        return;
+        // that.setData({
+        //     rgcData: {
+        //         address: '地址：' + data[i].address + '\n',
+        //         desc: '描述：' + data[i].desc + '\n',
+        //         business: '商圈：' + data[i].business
+        //     }
+        // });
     }
+
 })
-
-
-
-function getData(id, page) {
-
-    var arr = wx.getStorageSync('txt');
-
-    if (arr.length) {
-        arr.forEach((item) => {
-            if (item.id == id) {
-                page.setData({
-                    id: item.id,
-                    content: item.content
-                })
-            }
-        })
-    }
-
-}
-
-
-function setvalue(page) {
-
-    var arr = wx.getStorageSync('txt');
-
-    var data = [],
-        flag = true;
-
-    if (arr.length) {
-        arr.forEach((item) => {
-
-            if (item.id == page.data.id) {
-                item.time = Date.now();
-                item.content = page.data.content;
-                flag = false;
-            }
-            data.push(item);
-        })
-    }
-
-    if (flag) {
-
-        data.push(page.data);
-    }
-    wx.setStorageSync('txt', data);
-}
